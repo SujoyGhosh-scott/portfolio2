31cd206d0f3a54f636100bcbd1e433686610a6b6
SujoyGhosh-scott <ghoshsujoy336@gmail.com>
2023-02-03 21:47:20 +0530

    updated intro

--- components/Introduction/index.jsx
+++ components/Introduction/index.jsx
@@ -18,7 +18,10 @@ const Introduction = () => {
             A full-stack Web Developer
           </h2>
           <p className="pr-0 lg:w-3/4">
-            Currently pursuing masters in Computer Science from St. Xavier's College Kolkata. I'm really passionate about web development and blockchain. I want to use my skills and knowledge to develop applications that can impact people's lives.
+            Currently pursuing masters in Computer Science from St.
+            Xavier&apos;s College Kolkata. I&apos;m really passionate about web
+            development and blockchain. I want to use my skills and knowledge to
+            develop applications that can impact people&apos;s lives.
           </p>
 
           <a
